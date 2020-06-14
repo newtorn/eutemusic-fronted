43e48a3be9d80699e3c49ed85265dd92048fa518
newtorn <codertorn@gmail.com>
2020-06-14 19:51:29 +0800

    rebuild src to src/main src/renderer

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const path = require("path");
 const isProd = process.env.NODE_ENV === "production";
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 
 module.exports = {
   runtimeCompiler: true,
@@ -6,6 +11,94 @@ module.exports = {
   publicPath: isProd ? "./" : "/",
   productionSourceMap: true,
 
+  // webpack的相关配置
+  configureWebpack: {
+    entry: "./src/renderer/main.js",
+    resolve: {
+      extensions: [".js", ".vue", ".json", ".ts", ".less"],
+      alias: {
+        "@": resolve("src/renderer"),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(html)(\?.*)?$/,
+          use: "vue-html-loader",
+        },
+      ],
+    },
+    // 公共资源合并
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: "all",
+            test: /node_modules/,
+            name: "vendor",
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 100,
+          },
+          common: {
+            chunks: "all",
+            test: /[\\/]src[\\/]js[\\/]/,
+            name: "common",
+            minChunks: 2,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 60,
+          },
+          styles: {
+            name: "styles",
+            test: /\.(sa|sc|le|c)ss$/,
+            chunks: "all",
+            enforce: true,
+          },
+          runtimeChunk: {
+            name: "manifest",
+          },
+        },
+      },
+    },
+    // 性能警告修改
+    performance: {
+      hints: "warning",
+      // 入口起点的最大体积 整数类型（以字节为单位）
+      maxEntrypointSize: 50000000,
+      // 生成文件的最大体积 整数类型（以字节为单位 300k）
+      maxAssetSize: 30000000,
+      // 只给出 js 文件的性能提示
+      assetFilter: function(assetFilename) {
+        return assetFilename.endsWith(".js");
+      },
+    },
+  },
+
+  // 打包输出路径
+  outputDir: 'dist/web',
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: isProd,
+    // 开启 CSS source maps?
+    sourceMap: !isProd,
+    // css预设器配置项
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          'primary-color': '#c62f2f',
+          'link-color': '#c62f2f',
+          'border-radius-base': '4px'
+        },
+        javascriptEnabled: true
+      }
+    },
+    // 启用 CSS modules for all css / pre-processor files.
+    requireModuleExtension: true
+  },
+
+  // 第三方插件配置
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
@@ -66,9 +159,21 @@ module.exports = {
           createStartMenuShortcut: true,
         },
       },
-      outputDir: "dist",
-      mainProcessFile: "src/background.js",
-      mainProcessWatch: ["src"],
+      chainWebpackMainProcess: (config) => {
+        config.plugin("define").tap((args) => {
+          args[0]["IS_ELECTRON"] = true;
+          return args;
+        });
+      },
+      chainWebpackRendererProcess: (config) => {
+        config.plugin("define").tap((args) => {
+          args[0]["IS_ELECTRON"] = true;
+          return args;
+        });
+      },
+      outputDir: 'dist/electron',
+      mainProcessFile: "src/main/main.js",
+      mainProcessWatch: ["src/main"],
     },
   },
 };
