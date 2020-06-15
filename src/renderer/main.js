474c84df237789b32c38350305a159eed00180dc
newtorn <codertorn@gmail.com>
2020-06-16 00:32:24 +0800

    load styles

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueElectron from "vue-electron";
 import './plugins/ant-design-vue';
+import './styles/index.less';
 
 if (!process.env.IS_WEB) Vue.use(VueElectron);
 Vue.config.productionTip = false;
