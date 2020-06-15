2d0dbb5d0817a06b3227d35a7c0c9c411ef69b73
newtorn <codertorn@gmail.com>
2020-06-15 19:35:45 +0800

    add ant-design-vue

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueElectron from "vue-electron";
+import './plugins/ant-design-vue';
 
 if (!process.env.IS_WEB) Vue.use(VueElectron);
 Vue.config.productionTip = false;
---
+++ src/renderer/plugins/ant-design-vue.js
@@ -0,0 +1,6 @@
+import {
+
+} from 'ant-design-vue'
+
+
+/* v1.6.2+ registration methods */
