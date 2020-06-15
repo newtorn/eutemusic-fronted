7b7b3f7d046327f949f0ebac45486040f319c4da
newtorn <codertorn@gmail.com>
2020-06-16 03:02:54 +0800

    add component: controls

---
+++ src/renderer/components/controls/AppControls.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="icons-list">
+    <a-icon type="close-circle" theme="twoTone" two-tone-color="green" />
+    <a-icon type="minus-circle" />
+    <a-icon type="plus-circle" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.icons-list >>> .anticon {
+  font-size: 14px;
+}
+</style>
---
+++ src/renderer/components/controls/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="top-bar">
+      <sys-controls></sys-controls>
+      <app-controls></app-controls>
+  </div>
+</template>
+
+<script>
+import SysControls from "./SysControls";
+import AppControls from "./AppControls";
+export default {
+  components: {
+      SysControls,
+      AppControls
+  }
+};
+</script>
+
+<style>
+</style>
