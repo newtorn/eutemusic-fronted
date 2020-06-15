18332e6c2ae49280eff1556228ad3ab6b7552f25
newtorn <codertorn@gmail.com>
2020-06-15 19:04:05 +0800

    add initIpcEvent

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  methods: {},
+  created() {},
+  mounted() {
+    this.$electron.ipcRenderer.on("will-close", () => {
+      this.$electron.ipcRenderer.send("app-exit");
+    });
   }
-}
+};
 </script>
 
 <style>
