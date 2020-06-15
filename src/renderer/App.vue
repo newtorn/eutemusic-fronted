74524ebe621e133ec7b1939db5a14e781de8711f
newtorn <codertorn@gmail.com>
2020-06-16 00:25:16 +0800

    add #app style

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <controls></controls>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Controls from "./components/controls/Index";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Controls
   },
   methods: {},
   created() {},
@@ -25,11 +24,9 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  font-size: 0;
+  background-color: transparent;
 }
 </style>
